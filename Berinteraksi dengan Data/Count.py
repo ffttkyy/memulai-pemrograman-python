92e7c09d0ef26ec7799bd7c1c2f0d81a311782bb
Ky <128668501+ffttkyy@users.noreply.github.com>
2024-11-13 11:44:55 +0700

    Create Count.py

---
+++ Berinteraksi dengan Data/Count.py
@@ -0,0 +1,16 @@
+genap = [2,2,0,1,3,4,5,6,7,8,8,5,3,2,4,2,4,5,2,4,0]
+print(genap.count(2))
+
+"""
+Output:
+5
+"""
+
+string = "Hari ini panas banget"
+substring = "a"
+print(string.count(substring))
+
+"""
+Output:
+4
+"""
