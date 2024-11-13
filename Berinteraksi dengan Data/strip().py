c62d31ba362e212f455059bb463aca8d3cd421b5
Ky <128668501+ffttkyy@users.noreply.github.com>
2024-11-13 11:08:06 +0700

    Create strip().py

---
+++ Berinteraksi dengan Data/strip().py
@@ -0,0 +1,16 @@
+#strip()
+#Metode ini bertugas untuk menghapus whitespace pada bagian awal dan akhir string.
+
+print("    Dicoding    ".strip())
+
+"""
+Output:
+Dicoding
+"""
+
+kata = 'CodeCodeDicodingCodeCode'
+print(kata.strip("Code"))
+"""
+Output:
+Dicoding
+"""
