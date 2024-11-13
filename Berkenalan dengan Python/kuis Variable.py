3fd7f908deddd931e6c77ce23f5434e707e26016
Ky <128668501+ffttkyy@users.noreply.github.com>
2024-11-13 10:32:14 +0700

    Update kuis Variable.py

--- Berkenalan dengan Python/kuis Variable.py
+++ Berkenalan dengan Python/kuis Variable.py
@@ -1,1 +1,15 @@
-
+"""
+2
+TODO:
+3
+- Buatlah sebuah variabel bernama greeting.
+4
+- Assign teks "Saya mulai belajar Python!" pada variabel tersebut. 
+5
+"""
+6
+​
+7
+# TODO: Silakan buat kode Anda di bawah ini.
+8
+greeting = "Saya mulai belajar Python!"
