05a130b9771c5802670099fa9d5246a036035876
Ky <128668501+ffttkyy@users.noreply.github.com>
2024-11-13 11:25:58 +0700

    Create len().py

---
+++ Berinteraksi dengan Data/len().py
@@ -0,0 +1,14 @@
+#len()
+#Fungsi len() bertujuan untuk menghitung panjang atau banyaknya elemen dari list, set, dan string.
+#Khusus pada string, program akan menghitung jumlah karakternya.
+
+contoh_list = [1,3,5,2,3,5,2,5,2,4,2,2,0,1,3,3]
+
+print(contoh_list)
+print(len(contoh_list))
+
+"""
+Output:
+[1, 3, 5, 2, 3, 5, 2, 5, 2, 4, 2, 2, 0, 1, 3, 3]
+16
+"""
