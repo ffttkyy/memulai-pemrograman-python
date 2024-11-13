3070285824530faffc48dc50971e2542a5b4333f
Ky <128668501+ffttkyy@users.noreply.github.com>
2024-11-13 11:32:39 +0700

    Update len().py

--- Berinteraksi dengan Data/len().py
+++ Berinteraksi dengan Data/len().py
@@ -12,3 +12,25 @@ Output:
 [1, 3, 5, 2, 3, 5, 2, 5, 2, 4, 2, 2, 0, 1, 3, 3]
 16
 """
+
+contoh_list = set([1,3,5,2,3,5,2,5,2,4,2,2,0,1,3,3])
+
+print(contoh_list)
+print(len(contoh_list))
+
+"""
+Output:
+{0, 1, 2, 3, 4, 5}
+6
+"""
+
+contoh_list = "Belajar Python"
+print(contoh_list)
+print(len(contoh_list))
+
+
+"""
+Output:
+Belajar Python
+14
+"""
